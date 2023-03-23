cee6b9616097db37e8d9f78917edd06a225c56cf
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-03-23 18:21:49 +0530

    persisiting login

--- src/store/AuthContextProvider.js
+++ src/store/AuthContextProvider.js
@@ -3,16 +3,19 @@ import AuthContext from "./auth-context";
 
 
 const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null);
+    const initialToken = localStorage.getItem('token')
+    const [token, setToken] = useState(initialToken);
     const userIsLoggedIn = !!token;
   
     const loginHandler = (token) => {
       setToken(token);
+      localStorage.setItem('token', token);
       console.log(token)
     };
   
     const logoutHandler = () => {
       setToken(null);
+      localStorage.removeItem('token')
     };
     const contextValue = {
       token: token,
